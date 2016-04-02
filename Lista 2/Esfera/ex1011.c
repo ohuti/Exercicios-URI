e69748cd0dab090d71e7437d79b1b6e0ebb3e2d2
Andre Ohuti <ohuti@hotmail.com>
2016-04-02 15:05:28 -0300

    URI

---
+++ Lista 2/Esfera/ex1011.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+#define pi 3.14159
+
+int main(void) {
+	int R;
+	double VOL, 
+	scanf("%d", &R);
+	VOL = (4.0/3.0) * pi * pow(R, 3);
+	printf("VOLUME = %.4f\n", VOL);
+	return 0;
+}
